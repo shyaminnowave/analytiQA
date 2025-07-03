34e52e16a103eeb022b59abadcf2bbf58c79ef39
shyaminnowave <shyam.kumar@innowave.tech>
2025-07-03 10:45:39 +0530

    Natco firmware feature

--- apps/stb/apis/urls.py
+++ apps/stb/apis/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from apps.stb.apis.views import LanguageListView, LanguageDetailView, StbManufactureListView, \
     StbManufactureDetailView, NatCoAPIView, NatCoOptionView, LanguageOptionView, \
-    DeviceOptionView, NatCoDetailAPIView, NatcoInfoView, STBRunnerAPIView, NatCoReleaseOptionView
-
+    DeviceOptionView, NatCoDetailAPIView, NatcoInfoView, STBRunnerAPIView, NatCoReleaseOptionView, \
+    NatCoFirmwareDetailView, NatCoFirmwareView, NatCoFirmwareOptionView, GetSTBBranchAPI, STBRunnerAPIView, \
+    STBRunnerPackAPIView, STBTestcaseView, StbSchedulerView
 
 app_name = 'stb'
 
@@ -18,12 +19,28 @@ urlpatterns = [
     path('natCo/<int:pk>', NatCoDetailAPIView.as_view(), name="natCo-detail"),
 
     # NatCoRelease
-    path('natCo-release', NatcoInfoView.as_view()),
+    path('natco-releases', NatcoInfoView.as_view()),
+
+    # NatCoFirmware
+    path('firmware', NatCoFirmwareView.as_view(), name='natCo-firmware'),
+    path('firmware/<int:id>', NatCoFirmwareDetailView.as_view(), name='firmware-detail'),
+
+    # STB Branches API
+    path('branches', GetSTBBranchAPI.as_view(), name='get-stb-branch'),
+    path('stb-runner', STBRunnerPackAPIView.as_view(), name='get-stb-runner'),
+    path('stb-testcases', STBTestcaseView.as_view(), name='get-stb-testcases'),
+
+    # STB Runner API
+    path('stb-run', STBRunnerAPIView.as_view(),  name='stb-runner'),
+
+    # STB Scheduler API
+    path('stb-scheduler', StbSchedulerView.as_view(), name='stb-scheduler'),
 
     # Option API endpoints
     path('natCo-option', NatCoOptionView.as_view(), name='natCo-option'),
     path('language-option', LanguageOptionView.as_view(), name='language-option'),
     path('device-option', DeviceOptionView.as_view(), name='device-option'),
+    path('firmware-option', NatCoFirmwareOptionView.as_view(), name='firmware-option'),
     path('natco-release', NatCoReleaseOptionView.as_view(), name='natco-release'),
 
 ]
